78df4a1762d23a4aeec6caeeab618f4bc6d73d5b
MasterEagle <81796957+NeelSavsani@users.noreply.github.com>
2025-06-10 22:05:32 +0530

    Add files via upload

---
+++ Sender/Sender/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Sender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support — do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chooseFile = new System.Windows.Forms.Button();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.btnSendWhatsapp = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Controls.Add(this.label7, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtName, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtMobile, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.txtMessage, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.chooseFile, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.lblFilePath, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.btnSendWhatsapp, 1, 7);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Ubuntu Mono", 22F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label7.Location = new System.Drawing.Point(298, 6);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(204, 38);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "EageConnect";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Copperplate Gothic Bold", 10F);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label5.Location = new System.Drawing.Point(288, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(223, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "WhatsApp Messenger Tool";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ubuntu Mono", 11F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(109, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Receiver\'s Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtName.Font = new System.Drawing.Font("Ubuntu Mono Medium", 11F);
+            this.txtName.Location = new System.Drawing.Point(243, 85);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(314, 24);
+            this.txtName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ubuntu Mono", 11F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(125, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mobile Number";
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMobile.Font = new System.Drawing.Font("Ubuntu Mono Medium", 11F);
+            this.txtMobile.Location = new System.Drawing.Point(243, 120);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(314, 24);
+            this.txtMobile.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ubuntu Mono", 11F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(173, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Message";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMessage.Font = new System.Drawing.Font("Ubuntu Mono Medium", 11F);
+            this.txtMessage.Location = new System.Drawing.Point(243, 153);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(314, 94);
+            this.txtMessage.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Ubuntu Mono", 11F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(141, 258);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 18);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Upload File";
+            // 
+            // chooseFile
+            // 
+            this.chooseFile.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.chooseFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.chooseFile.Location = new System.Drawing.Point(243, 256);
+            this.chooseFile.Name = "chooseFile";
+            this.chooseFile.Size = new System.Drawing.Size(100, 23);
+            this.chooseFile.TabIndex = 9;
+            this.chooseFile.Text = "Choose File";
+            this.chooseFile.UseVisualStyleBackColor = true;
+            this.chooseFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFilePath.BackColor = System.Drawing.Color.White;
+            this.lblFilePath.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.lblFilePath.ForeColor = System.Drawing.Color.Black;
+            this.lblFilePath.Location = new System.Drawing.Point(243, 288);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(314, 18);
+            this.lblFilePath.TabIndex = 10;
+            // 
+            // btnSendWhatsapp
+            // 
+            this.btnSendWhatsapp.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSendWhatsapp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnSendWhatsapp.Font = new System.Drawing.Font("Century Schoolbook", 13F, System.Drawing.FontStyle.Bold);
+            this.btnSendWhatsapp.ForeColor = System.Drawing.Color.White;
+            this.btnSendWhatsapp.Location = new System.Drawing.Point(296, 357);
+            this.btnSendWhatsapp.Name = "btnSendWhatsapp";
+            this.btnSendWhatsapp.Size = new System.Drawing.Size(208, 46);
+            this.btnSendWhatsapp.TabIndex = 11;
+            this.btnSendWhatsapp.Text = "Send WhatsApp ✅";
+            this.btnSendWhatsapp.UseVisualStyleBackColor = false;
+            this.btnSendWhatsapp.Click += new System.EventHandler(this.btnSendWhatsapp_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            this.openFile.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EageConnect - WhatsApp Messenger Tool";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button chooseFile;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.Button btnSendWhatsapp;
+        private System.Windows.Forms.OpenFileDialog openFile;
+    }
+}
---
+++ Sender/Sender/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Sender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            txtMobile.KeyPress += TxtMobile_KeyPress;
+            txtMobile.KeyDown += TxtMobile_KeyDown;
+            txtMobile.MouseClick += TxtMobile_MouseClick;
+            txtMobile.TextChanged += TxtMobile_TextChanged;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtMobile.Text = "+91";
+            txtMobile.SelectionStart = txtMobile.Text.Length;
+            txtName.Focus();
+        }
+
+        private bool IsValidMobileNumber(string number)
+        {
+            // Strip the +91 prefix if present before validating
+            if (number.StartsWith("+91"))
+            {
+                number = number.Substring(3);
+            }
+            return Regex.IsMatch(number, @"^[6-9][0-9]{9}$");
+        }
+
+        private void btnSendWhatsapp_Click(object sender, EventArgs e)
+        {
+            string mobileNumber = txtMobile.Text.Trim();
+            string receiverName = txtName.Text.Trim();
+            string message = txtMessage.Text.Trim();
+            string filePath = lblFilePath.Text.Trim();
+
+            // Validate mobile number
+            if (!IsValidMobileNumber(mobileNumber))
+            {
+                MessageBox.Show("❌ Invalid mobile number.\nIt must be 10 digits and start with 6,7,8, or 9.",
+                    "Validation Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                txtMobile.Focus();
+                return;
+            }
+
+            // Validate receiver name
+            if (string.IsNullOrWhiteSpace(receiverName))
+            {
+                MessageBox.Show("❌ Receiver's name cannot be empty.",
+                    "Validation Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                txtName.Focus();
+                return;
+            }
+
+            // Validate that at least one of message or file path is provided
+            bool isMessageFilled = !string.IsNullOrWhiteSpace(message);
+            bool isFileSelected = !string.IsNullOrWhiteSpace(filePath) && File.Exists(filePath);
+
+            if (!isMessageFilled && !isFileSelected)
+            {
+                MessageBox.Show("❌ Either a message must be entered or a file must be selected.",
+                    "Validation Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                txtMessage.Focus();
+                return;
+            }
+
+            // Log file validation
+            if (isFileSelected)
+            {
+                Console.WriteLine("Selected file: " + filePath);
+            }
+
+            // Call Python function (always send message even if empty; file logic might be handled separately)
+            bool pythonSuccess = RunPythonFunction(mobileNumber, receiverName, message, filePath);
+
+            if (pythonSuccess)
+            {
+                MessageBox.Show($"✅ Message sent successfully to {receiverName} on {mobileNumber}.",
+                    "Success",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                // Clear the form
+                txtName.Text = string.Empty;
+                txtMobile.Text = "+91";
+                txtMessage.Text = string.Empty;
+                lblFilePath.Text = "No File Chosen";
+                txtMobile.SelectionStart = txtMobile.Text.Length;
+                txtName.Focus();
+            }
+            else
+            {
+                MessageBox.Show("❌ Failed to send message via Python script.",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private bool RunPythonFunction(string mobileNumber, string receiverName, string message, string filePath)
+        {
+            try
+            {
+                string pythonExe = @"C:\Users\neels\AppData\Local\Programs\Python\Python313\python.exe";
+                string scriptPath = @"D:\Neel\Code\Python\File sender\send_whatsapp_message.py";
+
+                // Escape quotes for safety
+                message = message.Replace("\"", "\\\"");
+                filePath = filePath.Replace("\"", "\\\"");
+
+                string args = $"\"{scriptPath}\" \"{mobileNumber}\" \"{receiverName}\" \"{message}\" \"{filePath}\"";
+
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = pythonExe,
+                    Arguments = args,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = false
+                };
+
+                using (Process process = Process.Start(psi))
+                {
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+
+                    if (!string.IsNullOrEmpty(output))
+                        Console.WriteLine("Python Output: " + output);
+
+                    if (!string.IsNullOrEmpty(error))
+                        Console.WriteLine("Python Error: " + error);
+
+                    if (process.ExitCode == 0)
+                    {
+                        return true; // Success!
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Python script exited with code {process.ExitCode}.\nError: {error}",
+                            "Python Script Error",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error running Python script: {ex.Message}",
+                    "Python Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // No additional code needed here, but you can customize if desired.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                Title = "Select a File",
+                Filter = "All Files (*.*)|*.*"
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lblFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            // Placeholder
+        }
+
+        // Enforce +91 lock and digit-only input
+        private void TxtMobile_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txtMobile.SelectionStart < 3)
+            {
+                // Block backspace/delete and non-navigation keys
+                e.Handled = true;
+                txtMobile.SelectionStart = txtMobile.Text.Length;
+                return;
+            }
+
+            // Allow only digits
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TxtMobile_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.Left || e.KeyCode == Keys.Back) && txtMobile.SelectionStart <= 3)
+            {
+                e.Handled = true;
+                txtMobile.SelectionStart = txtMobile.Text.Length;
+            }
+        }
+
+        private void TxtMobile_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (txtMobile.SelectionStart < 3)
+            {
+                txtMobile.SelectionStart = txtMobile.Text.Length;
+            }
+        }
+
+        private void TxtMobile_TextChanged(object sender, EventArgs e)
+        {
+            if (!txtMobile.Text.StartsWith("+91"))
+            {
+                txtMobile.Text = "+91";
+                txtMobile.SelectionStart = txtMobile.Text.Length;
+            }
+        }
+    }
+}
